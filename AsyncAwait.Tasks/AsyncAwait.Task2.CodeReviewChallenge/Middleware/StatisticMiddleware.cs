29b79f340ebf8a5e2dae2d6b5b36af8e4cb13a2a
Eduard Adaska <Eduard_Adaska@epam.com>
2023-04-19 23:56:45 +0300

    AsyncAwait: completed Task2

--- AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
+++ AsyncAwait.Tasks/AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
@@ -23,20 +23,15 @@ public class StatisticMiddleware
     {
         string path = context.Request.Path;
 
-        var staticRegTask = Task.Run(
-            () => _statisticService.RegisterVisitAsync(path)
-                .ConfigureAwait(false)
-                .GetAwaiter().OnCompleted(UpdateHeaders));
-        Console.WriteLine(staticRegTask.Status); // just for debugging purposes
+        await _statisticService.RegisterVisitAsync(path);
+        await UpdateHeaders();
 
-        void UpdateHeaders()
+        async Task UpdateHeaders()
         {
-            context.Response.Headers.Add(
-                CustomHttpHeaders.TotalPageVisits,
-                _statisticService.GetVisitsCountAsync(path).GetAwaiter().GetResult().ToString());
+            var visitsCount = await _statisticService.GetVisitsCountAsync(path);
+            context.Response.Headers.Add(CustomHttpHeaders.TotalPageVisits, visitsCount.ToString());
         }
 
-        Thread.Sleep(3000); // without this the statistic counter does not work
         await _next(context);
     }
 }
