7ac4c095a314b7b043ec310dc765176f8eb97ee8
Eduard Adaska <Eduard_Adaska@epam.com>
2023-06-22 22:27:47 +0300

    GOF: implemented adapter task

---
+++ GOF/GOF/Adapter/ElementsToContainerAdapter.cs
@@ -0,0 +1,16 @@
+﻿namespace GOF.Adapter
+{
+    public class ElementsToContainerAdapter<T> : IContainer<T>
+    {
+        private readonly IEnumerable<T> _elements;
+
+        public ElementsToContainerAdapter(IElements<T> elements)
+        {
+            _elements = elements.GetElements();
+        }
+
+        public IEnumerable<T> Items => _elements;
+
+        public int Count => _elements.Count();
+    }
+}
