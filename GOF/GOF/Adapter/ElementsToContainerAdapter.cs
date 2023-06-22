c92a1241bca9b8831dc7506defe2309d4cd5c538
Eduard Adaska <Eduard_Adaska@epam.com>
2023-06-22 22:33:47 +0300

    Simplified namespace

--- GOF/GOF/Adapter/ElementsToContainerAdapter.cs
+++ GOF/GOF/Adapter/ElementsToContainerAdapter.cs
@@ -1,16 +1,15 @@
-﻿namespace GOF.Adapter
+﻿namespace GOF.Adapter;
+
+public class ElementsToContainerAdapter<T> : IContainer<T>
 {
-    public class ElementsToContainerAdapter<T> : IContainer<T>
+    private readonly IEnumerable<T> _elements;
+
+    public ElementsToContainerAdapter(IElements<T> elements)
     {
-        private readonly IEnumerable<T> _elements;
-
-        public ElementsToContainerAdapter(IElements<T> elements)
-        {
-            _elements = elements.GetElements();
-        }
-
-        public IEnumerable<T> Items => _elements;
-
-        public int Count => _elements.Count();
+        _elements = elements.GetElements();
     }
+
+    public IEnumerable<T> Items => _elements;
+
+    public int Count => _elements.Count();
 }
